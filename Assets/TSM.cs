4ac68c2e9e34b73559cf99c0999409f134a12a1d
Paul Sinnett <paul.sinnett@gmail.com>
2018-04-03 13:23:20 +0100

    split into rings

---
+++ Assets/TSM.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TSM : MonoBehaviour {
+
+    public int numberOfCities = 10;
+    public float radius = 100.0f;
+    public Transform nodePrefab;
+    public LineRenderer linePrefab;
+    
+    void Start() {
+        List<Vector2> nodes = new List<Vector2>();
+        for (int i = 0; i < numberOfCities; ++i) {
+            Vector2 randomPosition = Random.insideUnitCircle * radius;
+            Instantiate(nodePrefab, randomPosition, Quaternion.identity);
+            nodes.Add(randomPosition);
+        }
+        // order by y so that the highest point is first        
+        nodes.Sort((a, b) => b.y.CompareTo(a.y));
+        // take the first point and create a ring starting at that point
+        while (nodes.Count > 0) {
+            List<Vector2> ring = new List<Vector2>();
+            var firstNode = nodes[0];
+            ring.Add(firstNode);
+            Vector2 currentDirection = Vector2.right;
+            Vector2 currentNode = firstNode;
+            bool skipFirst = true;
+            while (nodes.Count > 0) {
+                // find the next point in the ring
+                float smallestAngle = Mathf.Infinity;
+                float smallestDistance = Mathf.Infinity;
+                Vector2 nextNode = currentNode;
+                Vector2 nextNodeDirection = currentDirection;
+                foreach (var node in nodes) {
+                    if (skipFirst) {
+                        skipFirst = false;
+                        continue;
+                    }
+                    Vector2 nodeDirection = node - currentNode;
+                    float distance = Vector2.Distance(node, currentNode);
+                    if (distance > 0.0f) {
+                        float angle = Vector2.SignedAngle(currentDirection, nodeDirection);
+                        if (angle < 0.0f) {
+                            angle += 360.0f;
+                        }
+                        if (angle < smallestAngle || (angle == smallestAngle && distance < smallestDistance)) {
+                            smallestAngle = angle;
+                            smallestDistance = distance;
+                            nextNode = node;
+                            nextNodeDirection = nodeDirection;
+                        }
+                    }
+                    else {
+                        // catch duplicate node
+                        smallestAngle = 0.0f;
+                        smallestDistance = distance;
+                        nextNode = node;
+                        nextNodeDirection = currentDirection;
+                        break;
+                    }
+                }
+                if (ring.Count > 1) {
+                    Debug.Assert(smallestAngle < Mathf.Infinity);
+                    Debug.Assert(smallestDistance < Mathf.Infinity);
+                    Debug.Assert(nextNode != currentNode);
+                    Debug.Assert(nextNodeDirection != Vector2.zero);
+                }
+                ring.Add(nextNode);
+                nodes.Remove(nextNode);
+                currentNode = nextNode;
+                currentDirection = nextNodeDirection;
+                if (currentNode == firstNode) {
+                    break;
+                }
+            }
+
+            var line = Instantiate(linePrefab, Vector3.zero, Quaternion.identity);
+            line.positionCount = ring.Count;
+            List<Vector3> positions = new List<Vector3>();
+            foreach (var position in ring) {
+                positions.Add(position);
+            }
+            line.SetPositions(positions.ToArray());
+        }
+
+        //var line = Instantiate(linePrefab, Vector3.zero, Quaternion.identity);
+        //line.positionCount = nodes.Count;
+        //line.SetPositions(nodes.ToArray());
+    }
+}
